8015391eb82c89dcf96da0d19d5953250f197819
Kang Jisoon <wltnskjs@naver.com>
2022-09-08 20:08:08 +0900

    🚑 Fix wordpress_setup.sh
    - add exec php-fpm7

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -6,7 +6,7 @@
 #    By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 17:12:47 by jisokang          #+#    #+#              #
-#    Updated: 2022/09/07 17:16:26 by jisokang         ###   ########.fr        #
+#    Updated: 2022/09/08 17:14:37 by jisokang         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,3 +16,4 @@ tar -xzvf latest.tar.gz
 mkdir -p /var/www/html
 mv wordpress/ /var/www/html/
 chown -R www-data:www-data /var/www/html/wordpress
+exec php-fpm7 -F
