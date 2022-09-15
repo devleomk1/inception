897de6c3d9da6cf24c7702843e07768b399ca205
Kang Jisoon <wltnskjs@naver.com>
2022-09-15 20:49:47 +0900

    🩹 Simple fix for unexpexted sudo

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -6,7 +6,7 @@
 #    By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 17:12:47 by jisokang          #+#    #+#              #
-#    Updated: 2022/09/15 15:42:26 by jisokang         ###   ########.fr        #
+#    Updated: 2022/09/15 20:48:42 by jisokang         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,8 +19,8 @@
 sleep 2
 chown -R www-data:www-data /var/www/
 
-#if [ ! -f "/var/www/html/wordpress/index.php" ]; then
-if [ ! sudo -u www-data sh -c 'wp core is-installed' ]; then
+#if [ ! sudo -u www-data sh -c 'wp core is-installed' ]; then
+if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	sudo -u www-data sh -c " \
 	wp core download --locale=$WORDPRESS_LANG && \
 	wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8"
